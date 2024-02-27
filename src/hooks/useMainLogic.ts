e7c134dfe279a9cbf102f6753d0a9728725fd0ca
Ivan Mochalov <mochalov.ivan2015@mail.ru>
2024-02-27 09:39:46 +0300

    dev: add shuffle func

--- src/hooks/useMainLogic.ts
+++ src/hooks/useMainLogic.ts
@@ -1,4 +1,20 @@
 export const useMainLogic = () => {
-  console.log('logic')
+  type Dour = {
+    count: number
+    present: boolean
+  }
+  const arr = [
+    { count: 0, present: true },
+    { count: 1, present: false },
+    { count: 2, present: false },
+  ]
+  function shuffle(array: Dour[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1))
+      ;[array[i], array[j]] = [array[j], array[i]]
+    }
+  }
+  shuffle(arr)
+  console.log(arr)
   return null
 }
